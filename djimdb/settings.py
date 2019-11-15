a4d41f5df56808b434f95fbc292d4027ca640735
GEORGOUDAKIS Ioannis <ioannis.georgoudakis@intrasoft-intl.com>
2019-11-16 01:59:52 +0200

    Whitenoise and static fixed

--- djimdb/settings.py
+++ djimdb/settings.py
@@ -135,6 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'staticfiles')
-        ]
+# STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'staticfiles')
+#         ]
