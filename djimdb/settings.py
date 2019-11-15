24518937140dd26698ec47debf011ccc81e61016
darkstar <georgoudakis@live.com>
2019-11-15 18:54:48 +0200

    Added deployment parameters

--- djimdb/settings.py
+++ djimdb/settings.py
@@ -9,13 +9,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#_&9(3=4x=6+hx)&_79tdi)jh@m7jh-6y6txvk3(do0yg2%67s'
+with open(os.path.join(BASE_DIR, 'djimdb/secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+X_FRAME_OPTIONS = 'DENY'
+CSRF_COOKIE_SECURE = 'TRUE'
+SESSION_COOKIE_SECURE = 'TRUE'
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,5 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
