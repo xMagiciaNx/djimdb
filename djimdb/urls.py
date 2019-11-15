72b4f166b592a893f3b5dff3bd3a2b3feb5087cd
darkstar <georgoudakis@live.com>
2019-11-15 20:46:56 +0200

    Added deployment parameters

--- djimdb/urls.py
+++ djimdb/urls.py
@@ -1,15 +1,16 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Movies API",
+      title="Snippets API",
       default_version='v1',
-      description="Movies API endpoints",
+      description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -18,8 +19,9 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('admin/', admin.site.urls),
     path('auth/', obtain_auth_token),
     path('api/',include('djimdb_app.urls')),
