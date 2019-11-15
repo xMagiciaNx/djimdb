d17fd926c0da763ff5255d95106b388f021972e9
GEORGOUDAKIS Ioannis <ioannis.georgoudakis@intrasoft-intl.com>
2019-11-16 01:56:59 +0200

    Whitenoise and static fixed

--- djimdb/settings.py
+++ djimdb/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'djimdb.urls'
 
@@ -136,5 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'staticfiles')
         ]
